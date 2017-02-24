c03a184c2b15a2489a78a470a615dee92f678619
Tim Kladko <timmer@i.ua>
2017-02-24 12:16:48 +0200

    Add initials
    +implimentation initials more comfortable

---
+++ pset2/initials.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main (void)
+{
+    int i = 0;
+    
+    // get name
+    string name =  get_string();
+    // get leight of string
+    int n = strlen(name);
+    
+    if (name != NULL)
+    {
+        for (i = 0; i < n; i++)
+        {   
+            if ( i == 0 && !isspace(name[i]))
+            {
+                printf("%c", toupper(name[i]));
+            }    
+            else if(isspace(name[i-1]))
+            {
+                printf("%c", toupper(name[i]));
+            }
+        }
+        printf("\n");
+    }
+}
